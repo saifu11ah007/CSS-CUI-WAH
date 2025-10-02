07e263689948934dc7580f289e8045633e7a4cc8
saifu11ah007 <saifulah880@gmail.com>
2025-10-02 22:11:43 +0500

    Adding Login controller fixing logic

--- backend/routes/signupRouter.js
+++ backend/routes/signupRouter.js
@@ -5,17 +5,13 @@ const {
   sendOtp,
   verifyOtp,
   uploadUniId,
-  approveUniId,
 } = require('../controllers/signupController');
-const upload = require('../middleware/upload.js').default; // Default export from upload.js
+const upload = require('../middleware/upload.js').default;
 const saveToBlob = require('../middleware/upload.js').saveToBlob;
 
-
-//signup
 router.post('/auth/signup-init', signupInit);
 router.post('/auth/send-otp', sendOtp);
 router.post('/auth/verify-otp', verifyOtp);
-router.post('/auth/upload-id', upload, saveToBlob, uploadUniId); // Order: Parse multipart, save to Blob, then controller
-router.post('/auth/approve-id/:regNo', approveUniId);
+router.post('/auth/upload-id', upload, saveToBlob, uploadUniId);
 
 module.exports = router;
